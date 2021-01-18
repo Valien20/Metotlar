69c240d39bb9e4103987c57cc7e155cca4bc5869
Valien20 <sena.senturk.19@gmail.com>
2021-01-19 00:09:09 +0300

    deneme3

---
+++ Metotlar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 80;
+           
+          
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
